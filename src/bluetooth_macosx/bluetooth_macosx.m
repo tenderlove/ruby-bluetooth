9596535bfc40eb668f62bcd3d3964fa6db50e12b
Eric Hodel <drbrain@segment7.net>
2010-02-19 16:31:26 -0800

    Made the OS X bluetooth extension "work".

--- src/bluetooth_macosx/bluetooth_macosx.m
+++ src/bluetooth_macosx/bluetooth_macosx.m
@@ -1,43 +1,27 @@
-// Include the Ruby headers and goodies
+// Include the Ruby headers and goodies #import <ruby.h>
 #import <ruby.h>
-#import <rubyio.h>
-#import <rubysig.h>
-#import <util.h>
-#import <unistd.h>
-
-#import <Foundation/NSDictionary.h>
 
 #import <IOBluetooth/objc/IOBluetoothDeviceInquiry.h>
 #import <IOBluetooth/IOBluetoothUserLib.h>
 
 #import "bluetooth_macosx.h"
 
+struct bluetooth_device_struct {
+	VALUE addr;
+	VALUE name;
+};
+
 VALUE bt_module;
 VALUE bt_device_class;
 VALUE bt_devices_class;
 BOOL BUSY = false;
 
-// The initialization method for this module
-void Init_ruby_bluetooth()
-{
-    bt_module = rb_define_module("Bluetooth");
-    bt_device_class = rb_define_class_under(bt_module, "Device", rb_cObject);
-    bt_devices_class = rb_define_class_under(bt_module, "Devices", rb_cObject);
-    rb_define_singleton_method(bt_devices_class, "scan", bt_devices_scan, 0);
-    rb_define_singleton_method(bt_device_class, "new", bt_device_new, 2);
-    rb_define_attr(bt_device_class, "addr", Qtrue, Qfalse);
-    rb_define_attr(bt_device_class, "name", Qtrue, Qfalse);
-    rb_undef_method(bt_devices_class, "initialize");
-}
-
-// Create a Device, right now it only holds name and addr
-static VALUE bt_device_new(VALUE self, VALUE name, VALUE addr)
-{
+static VALUE bt_device_new(VALUE self, VALUE name, VALUE addr) {
     struct bluetooth_device_struct *bds;
 
     VALUE obj = Data_Make_Struct(self,
-                                 struct bluetooth_device_struct, NULL,
-                                 free, bds);
+            struct bluetooth_device_struct, NULL,
+            free, bds);
 
     rb_iv_set(obj, "@name", name);
     rb_iv_set(obj, "@addr", addr);
@@ -46,74 +30,142 @@ static VALUE bt_device_new(VALUE self, VALUE name, VALUE addr)
 }
 
 // Scan local network for visible remote devices
-static VALUE bt_devices_scan(VALUE self)
-{
+static VALUE bt_devices_scan(VALUE self) {
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
     BluetoothDeviceScanner *bds = [BluetoothDeviceScanner new];
+
     [bds startSearch];
-	while([bds isBusy]);
+
+    CFRunLoopRun();
+
+    [pool release];
+
     return [bds foundDevices];
 }
 
 @implementation BluetoothDeviceScanner
 
-- (void)    deviceInquiryDeviceFound:(IOBluetoothDeviceInquiry*)sender  device:(IOBluetoothDevice*)device
-{
-    const BluetoothDeviceAddress* addressPtr = [device getAddress];
-    deviceAddressString = [NSString stringWithFormat:@"[%02x:%02x:%02x:%02x:%02x:%02x]",
-	  addressPtr->data[0],
-      addressPtr->data[1],
-      addressPtr->data[2],
-      addressPtr->data[3],
-      addressPtr->data[4],
-      addressPtr->data[5]];
+- (void) deviceInquiryComplete:(IOBluetoothDeviceInquiry*)sender {
+    rb_p(rb_str_new2("complete"));
 
-	NSString* deviceNameString = [inDevice getName];
-    if( !deviceNameString ) {
-      deviceNameString = @"[unknown]";
-    }
-
-    VALUE bt_dev = bt_device_new(bt_device_class, rb_str_new2([deviceNameString UTF8String]),
-	  rb_str_new2([deviceAddressString UTF8String]));
-    rb_ary_push(_foundDevices, bt_dev);
+    CFRunLoopStop(CFRunLoopGetCurrent());
 }
 
--(IOReturn)startSearch
-{
-    IOReturn    status;
+- (void) deviceInquiryComplete:(IOBluetoothDeviceInquiry*)sender
+                        error:(IOReturn)error aborted:(BOOL)aborted {
+    rb_p(rb_str_new2("complete"));
 
-    [self   stopSearch];
+    CFRunLoopStop(CFRunLoopGetCurrent());
+}
 
-    _inquiry = [IOBluetoothDeviceInquiry    inquiryWithDelegate:self];
+- (void) deviceInquiryDeviceFound:(IOBluetoothDeviceInquiry*)sender {
+    rb_p(rb_str_new2("device found"));
+}
 
-	rb_ary_clear(_foundDevices);
+- (void) deviceInquiryDeviceFound:(IOBluetoothDeviceInquiry*)sender
+                          device:(IOBluetoothDevice*)device {
+    rb_p(rb_str_new2("device found"));
+//    const BluetoothDeviceAddress* addressPtr = [device getAddress];
+//
+//    NSString* deviceAddressString = [NSString stringWithFormat:@"[%02x:%02x:%02x:%02x:%02x:%02x]",
+//             addressPtr->data[0],
+//             addressPtr->data[1],
+//             addressPtr->data[2],
+//             addressPtr->data[3],
+//             addressPtr->data[4],
+//             addressPtr->data[5]];
+//
+//    VALUE bt_dev = bt_device_new(bt_device_class,
+//            rb_str_new2("unknown"),
+//            rb_str_new2([deviceAddressString UTF8String]));
+//
+//    rb_p(rb_str_new2([deviceAddressString UTF8String]));
+//
+//    rb_ary_push(_foundDevices, bt_dev);
+}
 
-    status = [_inquiry  start];
-    if( status == kIOReturnSuccess )
-    {
-    [_inquiry   retain];
+- (void) deviceInquiryDeviceNameUpdated:(IOBluetoothDeviceInquiry*)sender {
+    rb_p(rb_str_new2("name updated"));
+}
+
+- (void) deviceInquiryDeviceNameUpdated:(IOBluetoothDeviceInquiry*)sender
+                                 device:(IOBluetoothDevice*)device
+                       devicesRemaining:(uint32_t)devicesRemaining {
+    rb_p(rb_str_new2("name updated"));
+}
+
+- (void) deviceInquiryStarted:(IOBluetoothDeviceInquiry*)sender {
+    rb_p(rb_str_new2("started"));
+}
+
+- (void) deviceInquiryUpdatingDeviceNamesStarted:(IOBluetoothDeviceInquiry*)sender {
+    rb_p(rb_str_new2("updating names started"));
+}
+
+- (void) deviceInquiryUpdatingDeviceNamesStarted:(IOBluetoothDeviceInquiry*)sender
+                                devicesRemaining:(uint32_t)devicesRemaining {
+    rb_p(rb_str_new2("updating names started"));
+}
+
+- (IOReturn) startSearch {
+    IOReturn status;
+
+    [self stopSearch];
+
+    _foundDevices = rb_ary_new();
+
+    _inquiry = [IOBluetoothDeviceInquiry inquiryWithDelegate:self];
+
+    [_inquiry setInquiryLength: 5];
+    [_inquiry setUpdateNewDeviceNames: FALSE];
+
+    status = [_inquiry start];
+
+    if (status == kIOReturnSuccess) {
+        [_inquiry retain];
 
         _busy = TRUE;
     }
-    return( status );
+
+    rb_p(rb_str_new2("starting"));
+
+    return status;
 }
 
--(void)stopSearch
-{
-    if( _inquiry )
-    {
-    [_inquiry stop];
+- (void) stopSearch {
+    if (_inquiry) {
+        [_inquiry stop];
         [_inquiry release];
         _inquiry = nil;
+
+        rb_p(rb_str_new2("stopped"));
     }
 }
 
-- (void)    deviceInquiryComplete:(IOBluetoothDeviceInquiry*)sender error:(IOReturn)error   aborted:(BOOL)aborted
-{
-  _busy = FALSE;
+- (BOOL) isBusy {
+    return _busy;
 }
 
-- (BOOL) isBusy
-{
-  return _busy;
+- (VALUE) foundDevices {
+    return _foundDevices;
 }
 @end
+
+void Init_ruby_bluetooth() {
+    bt_module = rb_define_module("Bluetooth");
+
+    // Bluetooth::Devices
+    bt_devices_class = rb_define_class_under(bt_module, "Devices", rb_cObject);
+
+    rb_undef_method(bt_devices_class, "initialize");
+    rb_define_singleton_method(bt_devices_class, "scan", bt_devices_scan, 0);
+
+    // Bluetooth::Device
+    bt_device_class = rb_define_class_under(bt_module, "Device", rb_cObject);
+
+    rb_define_singleton_method(bt_device_class, "new", bt_device_new, 2);
+
+    rb_define_attr(bt_device_class, "addr", Qtrue, Qfalse);
+    rb_define_attr(bt_device_class, "name", Qtrue, Qfalse);
+}
+
--- src/extconf.rb
+++ src/extconf.rb
@@ -6,15 +6,29 @@ extension_name = 'ruby_bluetooth'
 
 dir_config(extension_name)
 
-case RUBY_PLATFORM
-  when /linux/
-    if have_library('bluetooth')
-      create_makefile(extension_name, 'bluetooth_linux')
-    end
-  when /darwin/
-      create_makefile(extension_name, 'bluetooth_macosx')
-  when /(win32|mingw32)/
-    if find_header('Ws2bth.h', 'c:\archiv~1\micros~2\include')
-      create_makefile(extension_name, 'bluetooth_win32')
-    end
+name = case RUBY_PLATFORM
+       when /linux/ then
+         abort 'could not find bluetooth library' unless
+           have_library 'bluetooth'
+
+         'linux'
+       when /(win32|mingw32)/
+         abort 'could not find Ws2bth.h' unless
+           find_header('Ws2bth.h', 'c:\archiv~1\micros~2\include')
+
+         'win32'
+       when /darwin/ then
+         $LDFLAGS << '-framework IOBluetooth'
+         'macosx'
+       else
+         abort "unknown platform #{RUBY_PLATFORM}"
+       end
+
+create_makefile 'ruby_bluetooth', "bluetooth_#{name}"
+
+if RUBY_PLATFORM =~ /darwin/ then
+  open 'Makefile', 'a' do |io|
+    io.write "\n.m.o:\n\t#{COMPILE_C}\n\n"
+  end
 end
+
