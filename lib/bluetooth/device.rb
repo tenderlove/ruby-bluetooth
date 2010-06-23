8b44188d60069bc11c9108635883bfa38740369f
Eric Hodel <drbrain@segment7.net>
2010-06-22 22:53:02 -0700

    Add Bluetooth::Device#to_s

--- lib/bluetooth/device.rb
+++ lib/bluetooth/device.rb
@@ -1,3 +1,9 @@
 module Bluetooth
-  Device = Struct.new :name, :address
+  Device = Struct.new :name, :address do
+
+    def to_s
+      "#{name} at #{address}"
+    end
+
+  end
 end
--- sample/scan.rb
+++ sample/scan.rb
@@ -2,8 +2,5 @@ require "ruby_bluetooth"
 
 a = Bluetooth::Devices.scan
 a.each { |device|
-  puts device.addr
-  puts device.name
-  p device
+  puts device
 }
-p a
