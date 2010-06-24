2c0f9de58c93075041c38a169b4a0e893ad99a24
Eric Hodel <drbrain@segment7.net>
2010-06-23 23:12:36 -0700

    Add a helpful comment

--- sample/name.rb
+++ sample/name.rb
@@ -1,6 +1,6 @@
 require 'bluetooth'
 
-address = ARGV.shift || abort("#{$0} address")
+address = ARGV.shift || abort("#{$0} address # look up a device with scan.rb")
 
 device = Bluetooth::Device.new address
 
