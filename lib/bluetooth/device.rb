a668c3a6f4bf42185bdcbd726c172bfc67257e9f
Eric Hodel <drbrain@segment7.net>
2010-06-23 23:12:06 -0700

    Some RDoc for Bluetooth::Device

--- lib/bluetooth/device.rb
+++ lib/bluetooth/device.rb
@@ -1,17 +1,36 @@
+##
+# A bluetooth device
+
 class Bluetooth::Device
+
+  ##
+  # The address of this device in XX-XX-XX-XX-XX-XX format
 
   attr_accessor :address
 
+  ##
+  # Sets the name of this device
+
   attr_writer :name
+
+  ##
+  # Creates a new Device with an +address+ and an optional +name+
 
   def initialize address, name = nil
     @address = address
     @name = name
   end
 
+  ##
+  # The bytes of this address
+
   def address_bytes
     @address.split('-').map { |c| c.to_i(16) }.pack 'C*'
   end
+
+  ##
+  # The name of this Device.  It will be automatically looked up if not
+  # already known.
 
   def name
     return @name if @name
@@ -23,7 +42,7 @@ class Bluetooth::Device
     @name
   end
 
-  def to_s
+  def to_s # :nodoc:
     "#{name} at #{address}"
   end
 
